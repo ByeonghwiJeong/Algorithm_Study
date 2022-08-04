ada2491b5c4fe93c14b963727a0bed0b1261d54e
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-05 00:24:00 +0900

    22-08-04 9663 NQueen

---
+++ 05_Study/15_백트래킹_22-07-20/05_9663.py
@@ -0,0 +1,37 @@
+'''
+대각선 / (positive diag)
+    -> r + c 일정
+대각선 \ (negative diag)
+    -> r - c 일정
+'''
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 7)
+
+N = int(input())
+col = set()
+posDiag = set()
+negDiag = set()
+res = 0
+
+def backtrack(r):
+    global res
+    if r == N:
+        res += 1
+        return
+
+    for c in range(N):
+        if c in col or (r + c) in posDiag  or (r - c) in negDiag:
+            continue
+        col.add(c)
+        posDiag.add(r + c)
+        negDiag.add(r - c)
+
+        backtrack(r + 1)
+
+        col.remove(c)
+        posDiag.remove(r + c)
+        negDiag.remove(r - c)
+
+backtrack(0)
+print(res)
