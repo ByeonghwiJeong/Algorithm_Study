dd98ca12582c76aed8884ca01820493ef8909095
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-05 20:27:33 +0900

    clear

--- 05_Study/17_누적합_22-08-01/04_10986.py
+++ 05_Study/17_누적합_22-08-01/04_10986.py
@@ -20,6 +20,43 @@ N개의 수가 주어진다.
     - 부분합 list선언
     - M에대한 부분합의 나머지 list
     - 나머지 같은 것끼리 Combination
+
+나머지가 같은 경우에서 0 0 0 1 1  2 2 
+3C2 2C2 2C2
+0 X X X X X 
+
+NC2
+n개의 
+
+5 3
+1 2 3 1 2
+
+5 10
+1 2 3 4
+1 3 6 10
+       0
+0 0 0 0 0 0 0 0 0 0 0 
+1
+2 1 0 1 0 0 1 0 0 0 0
+value  2 이상인경우
+nC2 2c2 = 1
+- -
+-   -
+-       -
+  -   -
+    -
+    -   -
+      - -
+4~5 
+0 1 3 6 7 9
+    -   -
+0 1 0 0 1 0
+    -   -
+M ~ M range(M)
+0 ~ M-1
+1 2 3 1 2
+index 0 1 2
+value 4 2 0
 '''
 import sys
 input = sys.stdin.readline
@@ -33,6 +70,7 @@ for i in _list:
     sum += i
     tmp = sum % M
     _remainders.append(tmp)
+# 0 1 0 0 1 0
 
 _dict = dict()
 for i in _remainders:
@@ -40,9 +78,18 @@ for i in _remainders:
         _dict[i] += 1
     else:
         _dict[i] = 1
-
+# dict { 0: 4,  1 :2 }
+'''
+for i in range(n):
+    sum += aList[i]
+    # 누적합의 나머지 종류별 개수 구하기
+    mList[sum % m] += 1
+'''
+# _list = [4, 2, 0]
+# 1
 ans = 0
 for i in _dict.values():
     ans += i * (i - 1) // 2
+    # iC2
 
 print(ans)   
