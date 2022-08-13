ebf719b261d1ca4357c6f2c445a99cf5e5c189ec
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-14 02:43:56 +0900

    22-08-13

--- 00_test.py
+++ 00_test.py
@@ -1,12 +1,11 @@
-x = [[0, 1, 1, 0],[0, 1, 1, 0],[0, 1, 0, 0],[0, 1, 0, 0]]
-print(x[2:0])
-tmp1 = [y[2:] for y in x[2:]]
-print(tmp1)
-tmp2 = [[0] * 2 for _ in range(2)]
-print(tmp2)
+from bisect import bisect_right, bisect_left
+x = [0, 2, 5 ,5 , 10]
+print('3l', bisect_left(x, 3)) #2
+print('3r', bisect_right(x, 3)) #2
+print('5l', bisect_left(x, 5)) #2 
+print('5r', bisect_right(x, 5)) #4
+print('0l', bisect_left(x, 0)) #
+print('0r', bisect_right(x, 0))
+print('10l', bisect_left(x, 10)) #
+print('10r', bisect_right(x, 10))
 
-print('0000000')
-if tmp1 == tmp2:
-    print('같다')
-else:
-    print('다르다')
--- 05_Study/22_이분탐색_22-08-12/02_10816.py
+++ 05_Study/22_이분탐색_22-08-12/02_10816.py
@@ -1,22 +1,28 @@
 '''
+< 2가지 풀이 >
 이분탐색은 어떤식으로 사용?
 '''
+from bisect import bisect_left, bisect_right
 
 N = int(input())
 _N = list(map(int, input().split()))
-# _N.sort()
+_N.sort()
 M = int(input())
 _M = list(map(int, input().split()))
 
-_dict = dict()
-for i in _N:
-    if i in _dict:
-        _dict[i] += 1
-    else:
-        _dict[i] = 1
+
+# _dict = dict()
+# for i in _N:
+#     if i in _dict:
+#         _dict[i] += 1
+#     else:
+#         _dict[i] = 1
+
+# for i in _M:
+#     if i in _dict:
+#         print(_dict[i])
+#     else:
+#         print(0)
 
 for i in _M:
-    if i in _dict:
-        print(_dict[i])
-    else:
-        print(0)
+    print(bisect_right(_N, i) - bisect_left(_N, i))
