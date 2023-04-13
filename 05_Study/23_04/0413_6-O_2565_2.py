fdaeed318eb9082aaf14e72da6045615ac360803
Byeonghwi Jeong <95831345+ByeonghwiJeong@users.noreply.github.com>
2023-04-13 22:30:21 +0900

    23-04-13

---
+++ 05_Study/23_04/0413_6-O_2565_2.py
@@ -0,0 +1,52 @@
+"""
+< 전깃줄 >
+https://www.acmicpc.net/problem/2565
+문제)
+- 두 전봇대 A, B 사이에 전깃줄 설치
+- 서로 교차하는 경우 발생
+- 전깃줄이 연결위치는 위에서 부터 차례대로 번호가 매겨져 있음
+- 남아있는 모든 전깃줄이 서로 교차하지 않도록 하려고 함
+- 최소 전깃줄 개수를 구하라
+"""
+from bisect import bisect_left
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+lines = [list(map(int, input().split())) for _ in range(N)]
+lines.sort(key=lambda x: x[0])
+dp = []
+for i, j in lines:
+    k = bisect_left(dp, j)
+    if k == len(dp):
+        dp.append(j)
+    else:
+        dp[k] = j
+print(N - len(dp))
+
+
+"""
+8
+1 8
+3 9
+2 2
+4 1
+6 4
+10 10
+9 7
+7 6
+---
+1 8
+2 2
+3 9
+4 1
+6 4
+7 6
+9 7
+10 10
+왼쪽과 오른쪽이 증가해야 교차하지않음
+sort 이후에 두번째에서 가장 긴 증가하는 수열 (LIS)
+2 4 6 7 10
+1 4 6 7 10
+"""
